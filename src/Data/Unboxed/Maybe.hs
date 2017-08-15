843816dae02a81ca6989509bc170acc36704e0dc
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2017-08-15 12:34:12 -0400

    making pattern synonyms strict in both directions

--- src/Data/Unboxed/Maybe.hs
+++ src/Data/Unboxed/Maybe.hs
@@ -16,17 +16,6 @@ import GHC.Types
 --import qualified  Prelude as Pre
 --import Data.Void
 
-{-
-theres some surprising desugaring  in pattern synonyms
---pattern MyJust :: a -> Maybe a
---pattern MyJust !a
-data MyMaybe a = JustC a | NothingC
-
-pattern MyJust :: a -> MyMaybe a
--- pattern MyJust !a = JustC a -- this fails
-pattern MyJust a = JustC !a -- this is fine
-
--}
 
 data family Maybe (x :: TYPE (r :: RuntimeRep))
 
@@ -34,10 +23,11 @@ data instance Maybe (a :: * ) where
   MaybeSum :: !(# a  | (# #) #) -> Maybe a
 
 pattern Just :: ( a :: *) -> Maybe a
-pattern Just a =  MaybeSum !(# a | #)
+pattern Just a <-  MaybeSum (# !a | #) where
+  Just !a = MaybeSum (# a | #)
 
 pattern Nothing :: forall (a :: *) .  Maybe a
-pattern Nothing = MaybeSum !(# | (# #) #)
+pattern Nothing = MaybeSum (# | (# #) #)
 
 type TypeUnlifted = TYPE 'UnliftedRep
 
@@ -45,4 +35,6 @@ data instance Maybe (x :: TYPE 'UnliftedRep) where
   MaybeSumU :: !(# x | (# #) #) -> Maybe (x :: TYPE 'UnliftedRep)
 
 pattern JustU :: forall (x :: TypeUnlifted) . x -> Maybe x
-pattern JustU  a = MaybeSumU !(#  a |  #)
+pattern JustU  a <- MaybeSumU (#  !a |  #) where
+  JustU !a = MaybeSumU (# a |   #)
+
